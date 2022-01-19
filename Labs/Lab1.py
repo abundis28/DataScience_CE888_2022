7ad1d32010c53c38c9944573ec70f7288d65efee
abundis28 <andresabundis@gmail.com>
2022-01-19 13:43:15 +0000

    Solution of question 2. Calculating standard deviation of list of numbers.

--- Labs/Lab1.py
+++ Labs/Lab1.py
@@ -1,12 +1,17 @@
 import numpy as np
 
-## Question 1
+#### QUESTION 1
 def list_to_array(myList):
     ## Your code goes here
     return np.array(myList)
+# list1 = [1,2,3,4]
+# convertedList = list_to_array(list1)
+# print(type(convertedList))
 
-list1 = [1,2,3,4]
-
-convertedList = list_to_array(list1)
-
-print(type(convertedList))
+#### QUESTION 2
+def stdev(numbers):
+    ## Your code goes here
+    numbers = np.array(numbers)
+    return numbers.std()
+# list1 = [1,2,3,4]
+# print(stdev(list1))
